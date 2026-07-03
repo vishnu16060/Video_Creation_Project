786973e3f2a313af00b4cd07c58db2a6780c399b
vishnu16060 <vv.vishnu2006@gmail.com>
2026-07-03 10:36:53 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import os
+import glob
+
+@Handling Moviepy version difference 
+try:
+   from moviepy.editor import imageClip,concatenate_videoclips,AudioFilesclip
+except importError:
+  from moviepy import ImageClip,concatenate_videoclips, AudioFilesClips
+  import yt_dlip
+
+#---1. INITIALIZE STATE -----
+if 
